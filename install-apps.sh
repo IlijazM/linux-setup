e85584373d5771490813ef44b37c373a00627288
IlijazM <mehmedovic.ilijaz@gmail.com>
2025-08-13 21:01:49 +0200

    update

--- install-apps.sh
+++ install-apps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if $YES_TO_ALL; then
+if [[ $YES_TO_ALL = true ]]; then
   ls apps | grep -v default | xargs -I{} sh "./apps/{}"
 else
   ls apps | grep -v default | xargs gum choose --no-limit --selected=$(cat apps/default) | xargs -I{} sh "./apps/{}"
