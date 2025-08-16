001cdb4ff3de3f165009264722f90372a7db4893
IlijazM <mehmedovic.ilijaz@gmail.com>
2025-08-16 10:13:36 +0200

    adds ansible setup with test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,26 +4,33 @@ set -e
 CID=""
 
 cleanup() {
-    docker stop $CID
-    docker rm $CID
+  echo "cleanup"
+  docker stop $CID
+  docker rm $CID
 }
 trap cleanup EXIT INT ERR
 
+docker stop -i ansible-test
 
 # Build test image
-docker build -t server-test -f tests/Dockerfile.test .
+docker build -t ansible-test -f Dockerfile.test tests
 
 # Start container in background
-CID=$(docker run -d -p 2222:22 server-test)
+CID=$(docker run -d --rm -p 4242:4242 -p 2222:22 --name ansible-test ansible-test)
 
 # Wait for sshd
 sleep 2
 
+echo "docker container has the id $CID"
+
 # Run Ansible
 ansible-playbook \
-  -i "all, ansible_port=2222 ansible_host=127.0.0.1 ansible_user=root ansible_password=root ansible_ssh_common_args='-o StrictHostKeyChecking=no'" \
+  -i "localhost," \
+  -e "ansible_port=2222 ansible_host=127.0.0.1 ansible_user=root ansible_ssh_common_args='-o StrictHostKeyChecking=no'" \
+  -c ssh \
   ansible/playbooks/main.yml
 
+sleep 10000000000
 
 # Run verification
-python3 tests/test_basic.py
+# python3 tests/test_basic.py
